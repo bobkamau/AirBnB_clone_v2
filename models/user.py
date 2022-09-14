481dd7f1b7761b429d011ef2199e8036ee2eeb03
bobkamau <robertmwangi369@gmail.com>
2022-09-14 11:46:33 -0700

    user.py

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" holds class User"""
+
+import models
+
+from models.base_model import BaseModel, Base
+
+from os import getenv
+
+import sqlalchemy
+
+from sqlalchemy import Column, String
+
+from sqlalchemy.orm import 
