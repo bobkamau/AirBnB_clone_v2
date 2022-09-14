64951b5c69c62f994d0e7ef3406130d87b469889
bobkamau <robertmwangi369@gmail.com>
2022-09-14 11:40:10 -0700

    review.py

---
+++ models/review.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+""" holds class Review"""
+
+import models
+
+from models.base_model import BaseModel, Base
+
+from os import getenv
+
+import sqlalchemy
+
+from sqlalchemy import Column, String, 
